0fb0a78aa8192db18ed807b61023d4585d54dff6
sraorao <srinivasarao.rao@gmail.com>
2019-02-16 13:09:14 +0000

    v2.0 release

---
+++ app/src/main/java/com/oxford/srao/animapp/TimeBinsActivity.java
@@ -0,0 +1,13 @@
+package com.oxford.srao.animapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimeBinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_bins);
+    }
+}
